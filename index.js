6cf57dc17245aa6b5fe2707295fc333986f902b7
Jason Hansen <jasohansen@gmail.com>
2024-07-10 14:57:17 -0600

    gauges

--- index.js
+++ index.js
@@ -172,69 +172,68 @@ function createGaugeChart(ctx, value, label, maxValue) {
         ],
     };
 
-    // const gaugeNeedle = {
-    //     id: "gaugeNeedle",
-    //     afterDatasetDraw(chart, args, options) {
-    //       const {
-    //         ctx,
-    //         config,
-    //         data,
-    //         chartArea: { top, right, bottom, left, width, height },
-    //       } = chart;
-    //       ctx.save();
-    //       const needleValue = data.datasets[0].needleValue;
-    //       const dataTotal = data.datasets[0].data.reduce((a, b) => a + b, 0);
+    const gaugeNeedle = {
+        id: "gaugeNeedle",
+        afterDatasetDraw(chart, args, options) {
+          const {
+            ctx,
+            config,
+            data,
+            chartArea: { top, right, bottom, left, width, height },
+          } = chart;
+          ctx.save();
+          const needleValue = data.datasets[0].needleValue;
+          const dataTotal = data.datasets[0].data.reduce((a, b) => a + b, 0);
       
-    //       if (needleValue <= 100) {
-    //         var angle = Math.PI + (1 / 200) * needleValue * Math.PI;
-    //         console.log(angle);
-    //       } else if (needleValue <= 10000) {
-    //         var angle =
-    //           Math.PI +
-    //           (1 / 200) * 100 * Math.PI +
-    //           ((1 / 200) * needleValue * Math.PI * 65) / 10000;
-    //       } else if (needleValue <= 1000000) {
-    //         var angle =
-    //           Math.PI +
-    //           (1 / 200) * 100 * Math.PI +
-    //           ((1 / 200) * 10000 * Math.PI * 65) / 10000 +
-    //           ((1 / 200) * needleValue * Math.PI * 35) / 1000000;
-    //       } else {
-    //         var angle = 0;
-    //       }
+          if (needleValue <= 100) {
+            var angle = Math.PI + (1 / 200) * needleValue * Math.PI;
+          } else if (needleValue <= 10000) {
+            var angle =
+              Math.PI +
+              (1 / 200) * 100 * Math.PI +
+              ((1 / 200) * needleValue * Math.PI * 65) / 10000;
+          } else if (needleValue <= 1000000) {
+            var angle =
+              Math.PI +
+              (1 / 200) * 100 * Math.PI +
+              ((1 / 200) * 10000 * Math.PI * 65) / 10000 +
+              ((1 / 200) * needleValue * Math.PI * 35) / 1000000;
+          } else {
+            var angle = 0;
+          }
       
-    //       const cx = width / 2;
-    //       const cy = chart._metasets[0].data[0].y;
+          const cx = width / 2;
+          const cy = chart._metasets[0].data[0].y;
       
-    //       //needle
-    //       ctx.translate(cx, cy);
-    //       ctx.rotate(angle);
-    //       ctx.beginPath();
-    //       ctx.moveTo(0, -2);
-    //       ctx.lineTo(height - ctx.canvas.offsetTop - 160, 0); // change 160 value if the needle size gets changed
-    //       ctx.lineTo(0, 2);
-    //       ctx.fillStyle = "#444";
-    //       ctx.fill();
-    //       //needle dot
-    //       ctx.translate(-cx, -cy);
-    //       ctx.beginPath();
-    //       ctx.arc(cx, cy, 5, 0, 10);
-    //       ctx.fill();
-    //       ctx.restore();
+          //needle
+          ctx.translate(cx, cy);
+          ctx.rotate(angle);
+          ctx.beginPath();
+          ctx.moveTo(0, -2);
+          ctx.lineTo(height - ctx.canvas.offsetTop - 160, 0); // change 160 value if the needle size gets changed
+          ctx.lineTo(0, 2);
+          ctx.fillStyle = "#444";
+          ctx.fill();
+          //needle dot
+          ctx.translate(-cx, -cy);
+          ctx.beginPath();
+          ctx.arc(cx, cy, 5, 0, 10);
+          ctx.fill();
+          ctx.restore();
       
-    //       //text
-    //       ctx.font = "20px Ubuntu";
-    //       ctx.fillStyle = "#444";
-    //       ctx.fillText(needleValue + " CPM", cx, cy + 50);
-    //       ctx.font = "10px Ubuntu";
-    //       ctx.fillText(0, 5, cy + 20);
-    //       ctx.fillText(100, cx, 90);
-    //       ctx.fillText("10k", cx + 185, 200); // change values if the position gets changed
-    //       ctx.fillText("1M", cx + 193, 320); // change values if the position gets changed
-    //       ctx.textAlign = "center";
-    //       ctx.restore();
-    //     },
-    //   };
+          //text
+          ctx.font = "20px Ubuntu";
+          ctx.fillStyle = "#444";
+          ctx.fillText(needleValue + " CPM", cx, cy + 50);
+          ctx.font = "10px Ubuntu";
+          ctx.fillText(0, 5, cy + 20);
+          ctx.fillText(100, cx, 90);
+          ctx.fillText("10k", cx + 185, 200); // change values if the position gets changed
+          ctx.fillText("1M", cx + 193, 320); // change values if the position gets changed
+          ctx.textAlign = "center";
+          ctx.restore();
+        },
+      };
 
       const config = {
         type: "doughnut",
@@ -255,7 +254,7 @@ function createGaugeChart(ctx, value, label, maxValue) {
             },
           },
         },
-        // plugins: [gaugeNeedle],
+        plugins: [gaugeNeedle],
       };
 
     new Chart(ctx, config);
