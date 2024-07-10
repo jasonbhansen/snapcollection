112059761ebaefabdac51dcf2cb571e3a15c388b
Jason Hansen <jasohansen@gmail.com>
2024-07-10 14:54:46 -0600

    gauges

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ function createGaugeChart(ctx, value, label, maxValue) {
               displayColors: false,
               callbacks: {
                 label: function (tooltipItem, data, value) {
-                  return tooltipItem.label + " " + value;
+                  return tooltipItem.label + " " + data;
                 },
               },
             },
